c93751b116b64128964e511721571567433e062a
juniwardani <juniwardani66@gmail.com>
2024-10-05 17:41:24 +0800

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -8,6 +8,142 @@ const data = [
         "PEMBIMBING BARU": "USTADZ SYARIF",
         "STATUS": "JILID QURAN"
     },
+
+    {
+        "NAMA": "M. ALFALIH",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (088807066644)"
+    }, 
+{
+        "NAMA": "FAUZAN",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (0811507215)"
+    }, 
+{
+        "NAMA": "M. KHALID AZKA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (08125101244)"
+    }, 
+{
+        "NAMA": "MUHAMMAL NATHAN",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (081255599595)"
+    }, 
+{
+        "NAMA": "RAISYA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (085249109252)"
+    }, 
+{
+        "NAMA": "M. RADEYA AL FARAS",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (082331111124)"
+    }, 
+{
+        "NAMA": "AISYAH HUMAIRA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (085251454310)"
+    }, 
+{
+        "NAMA": "KIRANA ALIFA HIBATLLAH",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (085345145942)"
+    }, 
+{
+        "NAMA": "ALIFA MAULIDA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (0887435460451)"
+    }, 
+{
+        "NAMA": "NUR SAFANIYAH",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (085753439876)"
+    }, 
+{
+        "NAMA": "M. BILLY FIRDAUS",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (085753439876)"
+    }, 
+{
+        "NAMA": "RAQILA NUR FARZAN",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (082350970301)"
+    }, 
+{
+        "NAMA": "M.FATIH RISKI B",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZ THOHIR",
+        "STATUS": "JILID I (082153912107)"
+    }, 
+{
+        "NAMA": "NIZAM SYARIF",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZAH DIAH",
+        "STATUS": "JILID II(082148576595)"
+    }, 
+{
+        "NAMA": "M. ZIKRI BAZILA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZAH DIAH",
+        "STATUS": "JILID II(082148576595)"
+    }, 
+{
+        "NAMA": "M. NENO HERDIANA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZAH EVI",
+        "STATUS": "JILID II(081545428261)"
+    }, 
+{
+        "NAMA": "M. AFKAR PRATAMA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "USTADZAH DINDA",
+        "STATUS": "JILID II(08125183637)"
+    }, 
+{
+        "NAMA": "RANNY",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "JUNI WARDANI",
+        "STATUS": "JILID III (085387619136)"
+    }, 
+{
+        "NAMA": "HANIA SYAKIRA",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "JUNI WARDANI",
+        "STATUS": "JILID III (082251917177)"
+    }, 
+{
+        "NAMA": "SALSABILA MUHTADIN",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "JUNI WARDANI",
+        "STATUS": "JILID IV (081349182724)"
+    }, 
+{
+        "NAMA": "PUTRI FENI",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "JUNI WARDANI",
+        "STATUS": "JILID IV (085219456012)"
+    }, 
+{
+        "NAMA": "SITI ZAFINA WP",
+        "PEMBIMBING AWAL": "SANTRI BARU",
+        "PEMBIMBING BARU": "JUNI WARDANI",
+        "STATUS": "JILID IV (081347288162)"
+    }, 
+
+
+    
     {
         "NAMA": "ANDI RIZKI DAFA RAMADHAN",
         "PEMBIMBING AWAL": "JUNI WARDANI",
